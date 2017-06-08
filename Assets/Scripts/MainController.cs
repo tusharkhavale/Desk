218f37330abeac7745e1f73a13a11fa43262057e
tusharkhavale <tushar2717@gmail.com>
2017-06-08 10:55:00 +0530

    First Commit

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainController : MonoBehaviour {
+
+	public static MainController controller;
+	public GameObject Frame;
+	public Button btnInfo;
+	public Button btnBuy;
+
+	void Awake()
+	{
+		controller = this;
+	}
+
+	public void OnClickMarker(Marker marker)
+	{
+		Frame.gameObject.SetActive (true);
+		switch (marker) 
+		{
+			case Marker.Bag:
+				break;
+
+			case Marker.Bottle:
+				break;
+
+			case Marker.Charger:
+				break;
+
+			case Marker.Mobile:
+				break;
+
+			case Marker.Monitor:
+				break;
+		}
+
+	}
+
+	public void OnClickInfo()
+	{
+		
+	}
+
+	public void OnClickBuy()
+	{
+		
+	}
+
+
+	public void OnClickClose()
+	{
+		Frame.SetActive (false);
+	}
+}
